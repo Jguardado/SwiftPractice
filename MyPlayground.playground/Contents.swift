ce9e0793e903160876221eb196fbf79bcba1a8a3
Juan <jguardado85@gmail.com>
2018-12-28 16:08:22 -0800

    playing around with swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+var daysOfWeek = 7.0
+
+var daysOfYear = 365.0
+
+daysOfYear / daysOfWeek
+
--- Swift fun/ViewController.swift
+++ Swift fun/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalTextField: UITextField!
+    
+    @IBOutlet weak var tipTextField: UITextField!
+    
+    @IBOutlet weak var myFirstLabel: UILabel!
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.green
+        myFirstLabel.text = "Gonna add a button"
     }
 
-
+    @IBAction func pressMeTapped(_ sender: Any) {
+        print(totalTextField.text!)
+        print(tipTextField.text!)
+        
+        let totalAmount = Double(totalTextField.text!)!
+        let tipPercent = Double(tipTextField.text!)! / 100.0
+        
+        let tipAmount = totalAmount * tipPercent
+        print(tipAmount)
+        
+        if tipAmount > 50 {
+            myFirstLabel.text = "You're projected tip amount is $\(tipAmount)!😤"
+        } else {
+            myFirstLabel.text = "You're projected tip amount is $\(tipAmount)"
+        }
+        
+        
+        
+    }
+    
 }
 
